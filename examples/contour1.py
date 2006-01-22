aeb5e1fea3da4e5546420de0efe2c1df95acf384
Mary Haley <haley@ucar.edu>
2006-01-22 14:46:07 +0000

    This example is the old FAQ example "logcon.py". I'm only checking in this example because I want to try a test of running "make all" in my webdoc/Examples directory.
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@8659 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ examples/contour1.py
@@ -0,0 +1,74 @@
+#
+#  File:
+#    contour1.py
+#
+#  Synopsis:
+#    Illustrates converting to a log axis.
+#
+#  Categories:
+#    Contouring
+#
+#  Author:
+#    Fred Clare
+#  
+#  Date of initial publication:
+#    February 2004
+#
+#  Description:
+#    This example shows how to convert a linear axis to a log axis. It
+#    also shows how to force PS/PDF output to be portrait.
+#
+#  Effects illustrated:
+#    o  Using scalar field resources to change the range of the Y axis.
+#    o  Using transformation resources to log the Y axis.
+#    o  Using wkOrientation to force a portrait orientation.
+# 
+#  Output:
+#    This example produces two visualizations:
+#      1.)  Contouring with two linear axes.
+#      2.)  Contouring with a log Y axis and a linear X axis.
+#
+#  Notes:
+#     
+
+import Ngl
+import Numeric
+
+#
+# Create some dummy data for the contour plot.
+#
+N=25
+ 
+T = Numeric.zeros((N,N),Numeric.Float0)
+ 
+jspn = Numeric.power(Numeric.arange(-12,13),2)
+ispn = Numeric.power(Numeric.arange(-12,13),2)
+
+for i in range(0,len(ispn)):
+  T[i,:] = (jspn + ispn[i]).astype(Numeric.Float0)
+
+T = 100.0 - Numeric.sqrt(64 * T)
+
+#
+# Force the orientation to be portrait for both frames.
+#
+rlist    = Ngl.Resources()
+wks_type = "ps"
+if(wks_type == "ps" or wks_type == "pdf"):
+  rlist.wkOrientation = "Portrait"      # For PS or PDF output only.
+wks = Ngl.open_wks(wks_type,"contour1",rlist)
+
+cnres = Ngl.Resources()
+cnres.sfYCStartV    = 10         # Define limits for Y axis.
+cnres.sfYCEndV      = 1000
+cnres.tiXAxisString = "linear"   # Label for X axis.
+cnres.tiYAxisString = "linear"   # Label for Y axis.
+
+contour = Ngl.contour(wks,T,cnres)  # Create and draw contour plot.
+
+cnres.tiYAxisString = "log"    # Label for Y axis.
+cnres.trYLog        = True     # Log scaling for Y axis
+
+contour = Ngl.contour(wks,T,cnres)  # Create and draw contour plot.
+
+Ngl.end()
