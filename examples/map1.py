94334a02ebabc02361068af9a1852f3ce292d9f5
Mary Haley <haley@ucar.edu>
2006-01-22 20:25:12 +0000

    Added a simple map example showing how to change the aspect ratio.
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@8661 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ examples/map1.py
@@ -0,0 +1,54 @@
+#
+#  File:
+#    map1.py
+#
+#  Synopsis:
+#    Illustrates how to change a map's aspect ratio.
+#
+#  Categories:
+#    maps only
+#
+#  Author:
+#    Fred Clare
+#  
+#  Date of initial publication:
+#    February 2004
+#
+#  Description:
+#    This example shows how to skew a map's aspect ratio.
+#
+#  Effects illustrated:
+#    o  Using mpShapeMode to allow a free aspect ratio.
+#    o  Changing the map projection.
+# 
+#  Output:
+#    This example produces two visualizations:
+#      1.) Cylindrical Equidistant projection
+#      2.) Mollweide projection
+#
+#  Notes:
+#     
+
+import Ngl
+
+wks_type = "ps"
+wks = Ngl.open_wks("x11","map1",rlist)
+
+projections = ["CylindricalEquidistant","Mollweide"]
+
+mpres              = Ngl.Resources()   # Indicate you want to set some
+                                       # resources.
+mpres.mpShapeMode  = "FreeAspect"      # Allow the aspect ratio to be
+                                       # changed.
+
+mpres.vpWidthF     = 0.8               # Make the aspect ratio square.
+mpres.vpHeightF    = 0.8
+
+mpres.pmTitleDisplayMode = "Always"    # Force the title to appear.
+
+for i in xrange(len(projections)):
+  mpres.mpProjection = projections[i]
+  mpres.tiMainString = projections[i]
+  map = Ngl.map(wks,mpres)
+
+Ngl.end()
