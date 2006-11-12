1e6e53887f54c77f45de73e22a55118ca4384c37
fred <fred>
2006-11-12 08:05:27 +0000

    Added generate_2d_array.  The doc string is in, remaining documentation to come.
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@9267 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- Ngl.py
+++ Ngl.py
@@ -965,6 +965,36 @@ def dshowalskewt(p,t,td,nlvls):
 def dpwskewt(td,p,n):
   return c_dpwskewt(td,p,n)
 
+#
+#  Globals for random number generator for generat_2d_array
+#
+dfran_iseq = 0
+dfran_rseq = [.749, .973, .666, .804, .081, .483, .919, .903,   \
+              .951, .960, .039, .269, .270, .756, .222, .478,   \
+              .621, .063, .550, .798, .027, .569, .149, .697,   \
+              .451, .738, .508, .041, .266, .249, .019, .191,   \
+              .266, .625, .492, .940, .508, .406, .972, .311,   \
+              .757, .378, .299, .536, .619, .844, .342, .295,   \
+              .447, .499, .688, .193, .225, .520, .954, .749,   \
+              .997, .693, .217, .273, .961, .948, .902, .104,   \
+              .495, .257, .524, .100, .492, .347, .981, .019,   \
+              .225, .806, .678, .710, .235, .600, .994, .758,   \
+              .682, .373, .009, .469, .203, .730, .588, .603,   \
+              .213, .495, .884, .032, .185, .127, .010, .180,   \
+              .689, .354, .372, .429                            \
+             ]
+
+#
+#  Random number generator for generate_2d_array.
+#
+def dfran():
+  global dfran_iseq
+  global dfran_rseq
+  dfran_iseq = dfran_iseq % 100
+  r = dfran_rseq[dfran_iseq]
+  dfran_iseq = dfran_iseq + 1
+  return r
+
 ################################################################
 #  
 #  Public functions in alphabetical order.
@@ -1946,6 +1976,75 @@ the vertices.
   """
   return gc_tarea(lat1, lon1, lat2, lon2, lat3, lon3, radius=radius) +  \
          gc_tarea(lat1, lon1, lat3, lon3, lat4, lon4, radius=radius)
+
+def generate_2d_array(dims, num_low, num_high, minv, maxv, seed=0):
+  """
+Generate smooth 2D arrays primarily for use in examples.
+
+array = generate_2d_array(dims, num_low, num_high, minv, maxv, seed=0)
+
+dims -- a list (or array) containing the dimensions of the
+        two-dimensional array to be returned.
+
+num_low, num_high -- Integers representing the approximate minimum 
+                     and maximum number of highs and lows that the 
+                     output array will have. They must be in the 
+                     range 1 to 25. If not, then they will be set to 
+                     either 1 or 25.
+
+minv, maxv -- The exact minimum and maximum values that the output array 
+              will have.
+
+iseed -- an optional argument specifying a seed for the random number
+         generator.  If iseed is outside the range 0 to 99, it will
+         be set to 0.
+  """  
+  global dfran_iseq
+  dfran_iseq = seed
+#
+#  Dims are reversed in order to get the same results as the NCL function.
+#
+  nx = dims[1]
+  ny = dims[0]
+  out_array = Numeric.zeros([nx,ny],Numeric.Float64)
+  tmp_array = Numeric.zeros([3,51],Numeric.Float64)
+  fovm = 9./float(nx)
+  fovn = 9./float(ny)
+  nlow = max(1,min(25,num_low))
+  nhgh = max(1,min(25,num_high))
+  ncnt = nlow + nhgh
+#
+  for k in xrange(ncnt):
+    tmp_array[0,k] = 1.+(float(nx)-1.)*dfran()
+    tmp_array[1,k] = 1.+(float(ny)-1.)*dfran()
+    if (k < num_low):
+      tmp_array[2,k] = -1.
+    else:
+      tmp_array[2,k] =  1.
+#
+  dmin =  1.e+36
+  dmax = -1.e+36
+  for j in xrange(ny):
+    for i in xrange(nx):
+      out_array[i,j] = 0.5*(minv + maxv)
+      for k in xrange(ncnt):
+        tempi = (fovm*(float(i+1)-tmp_array[0,k]))
+        tempj = (fovn*(float(j+1)-tmp_array[1,k]))
+        temp  = -(tempi*tempi + tempj*tempj)
+        if (temp >= -20.):
+          out_array[i,j] = out_array[i,j] +    \
+             0.5*(maxv-minv)*tmp_array[2,k]*math.exp(temp)
+      dmin = min(dmin,out_array[i,j])
+      dmax = max(dmax,out_array[i,j])
+                   
+  for j in xrange(ny):
+    for i in xrange(nx):
+      out_array[i,j] = \
+        (((out_array[i,j]-dmin)/(dmax-dmin))*(maxv-minv))+minv
+
+  del tmp_array
+
+  return Numeric.transpose(out_array,[1,0])
 
 def get_double(obj,name):
   return(NhlGetDouble(int_id(obj),name))
