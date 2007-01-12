0612dffd87422075dcc53fdf4f8a62bd37034eae
Mary Haley <haley@ucar.edu>
2007-01-12 05:47:07 +0000

    I've pretty much gone back to the original get_ma_fill_value function, with a few small logic changes. I think we need to accomodate both Numeric and numpy users, regardless of which module they've imported.
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@9405 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- paft/NglA.py
+++ paft/NglA.py
@@ -7,43 +7,34 @@ HAS_NUM                  = paf_version.HAS_NUM
 import fplib
 
 #
-# Test to make sure we can actually load numpy or Numeric, and
-# that we are dealing with a reasonable version.
+# Test to make sure we can actually load numpy or Numeric.
+# If HAS_NUM = 2, then user has imported numpy version of module.
+# If HAS_NUM = 1, then user has imported Numeric version of module.
 #
 if HAS_NUM == 2:
   try:
-    import numpy as Numeric
-# 
-# If we are dealing with a numpy version that is less than 1.0.0, then
-# check the version that PAF was built with against this version.
-#
-    if Numeric.__version__[0] == '0' and \
-       Numeric.__version__ < __array_module_version__:
-      print 'Warning: your version of numpy may be older than what PAF'
-      print 'was built with. You could have compatibility problems.'
-      print 'PAF was built with numpy version',__array_module_version__,'and you are'
-      print 'importing version',Numeric.__version__
+    import numpy
   except ImportError:
-    print 'Cannot find numpy, cannot proceed.'
-    print 'Perhaps you need to install the Numeric version of PAF instead.'
-    exit
+    print 'Cannot find numpy.'
+    print 'Will try Numeric, even though you have loaded numpy'
+    print 'version of this module.'
+    try:
+      import Numeric
+    except ImportError:
+      print 'Cannot find Numeric either, cannot proceed.'
+      exit
 else:
   try:
     import Numeric
-#
-# I decided to comment this section out, because a Numeric 24 version
-# of PyNGL seems to work okay with Numeric 23.x (23.8 anyway), and I'm
-# assuming this carries over for PAF as well.
-#
-#    if Numeric.__version__[0] != __array_module_version__[0]:
-#      print 'Warning: your version of Numeric is different from what PyNGL'
-#      print 'was built with. You may have compatibility problems.'
-#      print 'PyNGL was built with Numeric version',__array_module_version__
-#      print 'and you are importing version',Numeric.__version__
   except ImportError:
-    print 'Cannot find Numeric, cannot proceed'
-    print 'Perhaps you need to install the numpy version of PAF instead.'
-    exit
+    print 'Cannot find Numeric.'
+    print 'Will try numpy, even though you have loaded Numeric'
+    print 'version of this module.'
+    try:
+      import numpy
+    except ImportError:
+      print 'Cannot find numpy either, cannot proceed.'
+      exit
 
 #
 #  get_ma_fill_value(arr)
@@ -51,99 +42,48 @@ else:
 #       arr - any Python object
 #     output:
 #       Two values: type, fill_value
-#         if HAS_NUM == 1:
-#           if arr is a Numeric masked array:
-#             type = "MA" and fill_value is the fill value
-#           else arr is a numpy masked array:
-#             type = "num", fill_value is the fill value,
-#                     warning is printed
-#         elif HAS_NUM == 2:
-#           if arr is a numpy masked array:
-#             type = "num" and fill_value is the fill value
-#           else arr is a Numeric masked array:
-#             type = "MA", fill_value is the fill value,
-#                     warning is printed
+#         if arr is a Numeric masked array:
+#            type = "MA" and fill_value is the fill value
+#         if arr is a numpy masked array:
+#            type = "num" and fill_value is the fill value
+#         if arr is not a masked array
+#            type and fill_value are returned as None.
 #
 def get_ma_fill_value(arr):
-#
-# If HAS_NUM = 1, this means the Numeric version of this module
-# was imported, and hence we should try the Numeric MA module first.
-#
-  if HAS_NUM == 1:
-    try:
-      import MA
-      fv = None
+  try:
+    import MA
+    fv = None
 #
 #  If arr is a Numeric masked array, return its fill value
 #
-      if (type(arr) == type(MA.array([0]))):
-        fv = arr.fill_value()
+    if (type(arr) == type(MA.array([0]))):
+      fv = arr.fill_value()
 #
 #  For Numeric 2.4 or later, the fill value is returned as
 #  a single-element array.  For Numeric releases prior to
 #  2.4, the fill value is returned as a numeric value, so
 #  return it.
 #
-        try:
-          len(fv)
-          return "MA",fv[0]
-        except:
-          return "MA",fv
-    except:
+      try:
+        len(fv)
+        return "MA",fv[0]
+      except:
+        return "MA",fv
+  except:
+    pass
 #
 #  If not a Numeric masked array, try for NumPy masked array.
-#  The user really shouldn't be doing this, however, b/c
-#  they've imported the Numeric version of the module.
-#  We will print a warning in this case.
 #
-      try:
-        import numpy.core.ma
-        if (type(arr) == type(numpy.core.ma.array([0]))):
-          "Warning: you've imported the Numeric version of PAF, but are using numpy arrays"
-          return "num",arr.fill_value()
-      except:
-        return None,None
-#
-# If HAS_NUM=2, this means the NumPy version of this module
-# was imported, and hence we should try the NumPy MA module first.
-#
-  elif HAS_NUM == 2:
+  try:
     "Try numpy"
-    try:
-      import numpy.core.ma
-      if (type(arr) == type(numpy.core.ma.array([0]))):
-        return "num",arr.fill_value()
-    except:
+    import numpy.core.ma
+    if (type(arr) == type(numpy.core.ma.array([0]))):
+      return "num",arr.fill_value()
+  except:
+    pass
 #
-#  If not a NumPy masked array, try for Numeric masked array.
-#  The user shouldn't be doing this, however, b/c they've
-#  imported the numpy version of the module. We will print a
-#  warning.
+#  Neither a Numeric nor a NumPy masked array.
 #
-      try:
-        import MA
-        fv = None
-#
-#  If arr is a Numeric masked array, return its fill value
-#
-        if (type(arr) == type(MA.array([0]))):
-          "Warning: you've imported the NumPy version of PAF, but are using Numric arrays"
-          fv = arr.fill_value()
-#
-#  For Numeric 24 or later, the fill value is returned as
-#  a single-element array.  For Numeric releases prior to
-#  24, the fill value is returned as a numeric value, so
-#  return it.
-#
-          try:
-            len(fv)
-            return "MA",fv[0]
-          except:
-            return "MA",fv
-        else:
-          return None,None
-      except:
-        return None,None
   return None, None
 
 def chiinv(x,y):
