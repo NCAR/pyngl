84344dbc30b89ab351df6ba99a950ad59a0a858e
Mary Haley <haley@ucar.edu>
2007-01-18 06:20:12 +0000

    I added a bunch of little logic functions, like is_numpy, is_numeric, is_numpy_scalar, is_numeric_scalar, is_python_scalar, and is_scalar.
    I also added a promote_scalar routine, as Fred and I discussed. This
    routine converts arguments to numeric or numpy arrays of at least 1
    dimension. This is to avoid the problem of getting an input argument
    that has an ndims of 0, and hence potentially causing the C extension
    module to fail.
    
    Whether to promote to a numeric or numpy array is determined first via
    the type of fplib module being loaded, and if that fails, then the
    other type is used.
    
    I then modified the "chiinv" interface to utilize these functions.
    
    The return argument of chiinv is now based on what types of arguments
    were passed into the function. If either of the input arguments are
    numpy arrays, then a numpy array is returned. Otherwise, if any of the
    input arguments are numeric arrays, then a numeric array is returned.
    If neither of the arguments are numeric or numpy arrays, then the type
    is determined by the type of the fplib module imported.
    
    Fred, we can discuss this logic tomorrow to see if you agree with it.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@9418 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- paft/NglA.py
+++ paft/NglA.py
@@ -11,6 +11,16 @@ import fplib
 # If HAS_NUM = 2, then user has imported numpy version of module.
 # If HAS_NUM = 1, then user has imported Numeric version of module.
 #
+# However, loading a Numeric module doesn't preclude the user from
+# using numpy arrays (and vice versa). We don't recommend this, but
+# we believe that it can be allowed.
+#
+# Keep track of which array module we were successful in loading,
+# as we will use this information later for determining whether
+# to create Numeric or numpy arrays to pass to the extension module
+# routiines.
+#
+recommend_numeric = False
 if HAS_NUM == 2:
   try:
     import numpy
@@ -20,12 +30,14 @@ if HAS_NUM == 2:
     print 'version of this module.'
     try:
       import Numeric
+      recommend_numeric = True
     except ImportError:
       print 'Cannot find Numeric either, cannot proceed.'
       exit
 else:
   try:
     import Numeric
+    recommend_numeric = True
   except ImportError:
     print 'Cannot find Numeric.'
     print 'Will try numpy, even though you have loaded Numeric'
@@ -86,9 +98,122 @@ def get_ma_fill_value(arr):
 #
   return None, None
 
-def chiinv(x,y):
-  return fplib.chiinv(x,y)
+#
+# This function returns True if it encounters a Python scalar.
+#
+def is_python_scalar(arg):
+  import types
+  if (type(arg)==types.IntType or type(arg)==types.LongType or \
+      type(arg)==types.FloatType):
+    return True
+  else:
+    return False
 
+# 
+# This function returns True if we have a Numeric array.
+#
+def is_numeric(arg):
+  try:
+    import Numeric
+    if (type(arg) == type(Numeric.array([0]))):
+      return True
+    else:
+      return False
+  except:
+    return False
+
+# 
+# This function returns True if we have a numpy scalar or array.
+#
+def is_numpy(arg):
+  try:
+    import numpy
+    if (type(arg) == type(numpy.array([0])) or isinstance(arg,numpy.generic)):
+      return True
+    else:
+      return False
+  except:
+    return False
+
+#
+# This function returns True if it encounters a numeric scalar.
+# A numeric scalar is a numeric array with 0 dimensions.
+#
+def is_numeric_scalar(arg):
+  if (is_numeric(arg) and (len(arg.shape) == 0)):
+    return True
+  else:
+    return False
+
+#
+# This function returns True if it encounters a numpy scalar.
+# A numpy scalar can either be a numpy array with 0 dimensions,
+# or a numpy scalar, which is a new thing that didn't exist in
+# numeric.
+#
+def is_numpy_scalar(arg):
+  try:
+    import numpy
+    if (type(arg) == type(numpy.array([0]))) and (len(arg.shape) == 0):
+      return True
+#
+# Test for numpy scalar.
+#
+    elif isinstance(arg,numpy.generic):
+      return True
+    else:
+      return False
+  except:
+    return False
+
+#
+# This function returns True if it's a Python scalar, a
+# numpy scalar, or a numeric scalar.
+#
+def is_scalar(arg):
+  return is_numeric_scalar(arg) or is_numpy_scalar(arg) or \
+         is_python_scalar(arg)
+#
+# Special function to deal with values that may come in as
+# a scalar (as defined by the "is_scalar" function above)
+# that need to be converted to something that won't
+# register as having 0 dimensions.  We do this by 
+# promoting it to a Numeric array.  Note that "Numeric"
+# may actually be "numpy", depending on whether the
+# Numeric or numpy module was imported. 
+#
+def promote_scalar(x):
+  if is_scalar(x):
+    if recommend_numeric:
+      import Numeric
+      return Numeric.array([x])
+    else:
+      import numpy
+      return numpy.array([x])
+  else:
+    return x
+
+def chiinv(x,y):
+#
+# Promote x and y to Numeric (or numpy) arrays that have at least
+# a dimension of 1.
+#
+  x2 = promote_scalar(x)
+  y2 = promote_scalar(y)
+#
+# Determine what kind of array to return. This is dependent on the
+# types of the arguments passed to chiinv, and not which fplib
+# module was loaded. Note that numpy is favored over Numeric.
+#
+  if is_numpy(x) or is_numpy(y):
+    import numpy
+    return numpy.array(fplib.chiinv(x2,y2))
+  elif is_numeric(x) or is_numeric(y):
+    import Numeric
+    return Numeric.array(fplib.chiinv(x2,y2))
+  else:
+    return fplib.chiinv(x2,y2)
+    
 def linmsg(x, end_pts_msg=0, max_msg=None, fill_value=1.e20):
 #
 #  If max_msg is the default, then set it to "0" which will
