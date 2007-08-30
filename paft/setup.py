6312208b1973c06ffc00825b7c108cbe08e73237
Mary Haley <haley@ucar.edu>
2007-08-30 02:27:38 +0000

    I had updated this in June to default to a NumPy build, and never checked it in.
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@9801 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- paft/setup.py
+++ paft/setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 #
 # This script is for building and installing the PyNGL analysis functions.
-# They will get installed in the PAF package directory (for Numeric) and
-# PAF_numpy (for numpy). Right now, I don't think the numpy version works.
+# They will get installed in the PAF package directory (for NumPy) and
+# PAF_numeric (for Numeric). It is not our intention to support Numeric,
+# however, so building a Numeric version is highly at your own risk.
 #
 # Eventually, this stuff will probably be combined with PyNGL, and become
 # part of the PyNGL build system.
@@ -19,16 +20,16 @@
 #
 #   python setup.py bdist_dumb --relative
 #
-# If no environment variables are set, this script will create
-# a Numeric version of PAF.
+# If no environment variables are set, this script will create a
+# NumPy version of PAF.
 #
-# There are four environment variables that if set, will change
+# There are environment variables that if set, will change
 # the behavior of this script:
 #
-#    USE_NUMPY     - Create a numpy version of PAF only.
+#    USE_NUMERIC   - Create a Numeric version of PAF only.
 #
-#    USE_NUMERPY   - Create a Numeric *and* numpy version of PAF. This
-#                    will create two packages: PAF and PAF_numpy.
+#    USE_NUMERPY   - Create a Numeric *and* NumPy version of PAF. This
+#                    will create two packages: PAF and PAF_numeric.
 #
 import sys,os,re
 import fileinput
@@ -36,39 +37,42 @@ import shutil
 from distutils.core import setup, Extension
 
 #
-# Determine whether we want to build a Numeric and/or Numpy version
-# of PAF.  If the environment variable USE_NUMPY is set, it will
-# try to build a numpy version. USE_NUMPY doesn't need to be set to
+# Determine whether we want to build a Numeric and/or NumPy version
+# of PAF.  Warning: it is not our intention to support Numeric, so
+# building a Numeric version is at your own risk.
+#
+# If the environment variable USE_NUMERIC is set, it will try to
+# build a Numeric version. USE_NUMERIC doesn't need to be set to
 # any value; it just has to be set.  If USE_NUMERPY is set, then
-# both versions of PAF will be built, and the Numeric version will
-# be put in package PAF, and the numpy version in package PAF_numpy.
+# both versions of PAF will be built, and the NumPy version will
+# be put in package PAF, and the Numeric version in package PAF_numeric.
 #
-# HAS_NUM will be set by this script depending on USE_NUMPY and USE_NUMERPY.
+# HAS_NUM will be set by this script depending on USE_NUMERIC and USE_NUMERPY.
 #
-# HAS_NUM = 3 --> install both numpy and Numeric versions of PAF
-# HAS_NUM = 2 --> install numpy version of PAF
+# HAS_NUM = 3 --> install both NumPy and Numeric versions of PAF
+# HAS_NUM = 2 --> install NumPy version of PAF
 # HAS_NUM = 1 --> install Numeric version of PAF
-# HAS_NUM = 0 --> You're hosed as numpy or Numeric don't exist.
+# HAS_NUM = 0 --> You're hosed as NumPy or Numeric don't exist.
 #
 try:
   path = os.environ["USE_NUMERPY"]
   HAS_NUM = 3
 except:
   try:
-    path = os.environ["USE_NUMPY"]
-    HAS_NUM = 2
-  except:
+    path = os.environ["USE_NUMERIC"]
     HAS_NUM = 1
+  except:
+    HAS_NUM = 2
 
 #
-# Test to make sure we actually the Numeric and/or numpy modules
+# Test to make sure we actually the Numeric and/or NumPy modules
 # that we have requested.
 #
 if HAS_NUM > 1:
   try:
     import numpy
   except ImportError:
-    print "Cannot find numpy; we'll try Numeric."
+    print "Cannot find NumPy; we'll try Numeric."
     HAS_NUM = 1
 
 if HAS_NUM == 1 or HAS_NUM == 3:
@@ -85,7 +89,7 @@ elif HAS_NUM == 2:
 elif HAS_NUM == 1:
   array_modules = ['Numeric']
 else:
-  print "Cannot find Numeric or numpy; good-bye!"
+  print "Cannot find Numeric or NumPy; good-bye!"
   sys.exit()
 
 #
@@ -106,7 +110,7 @@ ncl_inc       = os.path.join(ncl_root,'include')
 ncl_and_sys_lib_paths = [ncl_lib, "/usr/X11R6/lib"]
 
 if sys.platform == "darwin":
-    dirs = ['/sw/lib','/Users/haley/lib/gcc-lib/i386-apple-darwin8.6.1/4.0.3']
+    dirs = ['/sw/lib','/Users/haley/lib/gcc-lib/i386-apple-darwin8.9.1/4.0.3']
     for dir in dirs:
       if(os.path.exists(dir)):
         ncl_and_sys_lib_paths.append(dir)
@@ -130,7 +134,7 @@ if sys.platform == "aix5":
 # and needs f95 loaded.
 #
 if sys.platform == "darwin":
-    dir = '/Users/haley/lib/gcc-lib/i386-apple-darwin8.6.1/4.0.3'
+    dir = '/Users/haley/lib/gcc-lib/i386-apple-darwin8.9.1/4.0.3'
     if dir in ncl_and_sys_lib_paths:
       LIBRARIES.remove('g2c')
       LIBRARIES.append('f95')
@@ -153,22 +157,22 @@ for array_module in array_modules:
   if array_module == 'Numeric':
     from Numeric import  __version__ as array_module_version
 
-    paf_pkg_name = 'PAF'
-    paf_pth_file = [paf_pkg_name + '.pth']
+    paf_pkg_name = 'PAF_numeric'
+    paf_pth_file = []              # No *.pth file for Numeric package!
 
-    print '====> Installing the numeric version of PAF to the',paf_pkg_name,'package directory.'
+    print '====> Installing the Numeric version of PAF to the',paf_pkg_name,'package directory.'
 
     DMACROS       =  [('NeedFuncProto',None)]
   else:
     from numpy import __version__ as array_module_version
 
-    paf_pkg_name = 'PAF_numpy'
-    paf_pth_file = []              # No *.pth file for numpy package!
+    paf_pkg_name = 'PAF'
+    paf_pth_file = [paf_pkg_name + '.pth']
 
-    print '====> Installing the numpy version of PAF to the',paf_pkg_name,'package directory.'
+    print '====> Installing the NumPy version of PAF to the',paf_pkg_name,'package directory.'
 
 #
-# For a numpy build, we need to point to the correct array "arrayobject.h"
+# For a NumPy build, we need to point to the correct array "arrayobject.h"
 # and set the USE_NUMPY macro for compiling the *.c files.
 #
     INCLUDE_PATHS.insert(0,os.path.join(pkgs_pth,"numpy/core/include"))
@@ -189,7 +193,7 @@ for array_module in array_modules:
 
 #
 # This seems kludgy to me, but I need to make sure that if both 
-# numpy and Numeric versions of PAF are being built, we clean
+# NumPy and Numeric versions of PAF are being built, we clean
 # the *.o files beforehand. This is because "setup" puts the *.o files
 # in the same location (build/temp.xxxx/.) every time, regardless of which
 # package we're building. Maybe there's a way to tell setup to put the
@@ -204,7 +208,7 @@ for array_module in array_modules:
 # Get version number.
 #
   if os.path.exists(paf_vfile):
-    os.system("/bin/rm -rf " + paf_vfile)
+    os.remove(paf_vfile)
 
   paf_version = open('version','r').readlines()[0].strip('\n')
   vfile = open(paf_vfile,'w')
@@ -212,7 +216,7 @@ for array_module in array_modules:
   vfile.write("array_module = '%s'\n" % array_module)
 #
 # The Ngl.py and Nio.py files use HAS_NUM to tell whether to use
-# Numeric or numpy specific operations.
+# Numeric or NumPy specific operations.
 #
   if array_module == 'Numeric':
     vfile.write("HAS_NUM = 1\n")
@@ -245,4 +249,4 @@ for array_module in array_modules:
 # Clean up. Remove version file.
 #
 if os.path.exists(paf_vfile):
-  os.system("/bin/rm -rf " + paf_vfile)
+  os.remove(paf_vfile)
