20092f1157c6a237644b3a659e98d6fa5d7f1e9c
Mary Haley <haley@ucar.edu>
2008-08-13 03:49:33 +0000

    Cleaned up this setup.py to add F2CLIBS and F2CLIBS_PREFIX like I did for the PyNIO setup.py.
    This eliminates the need for a bunch of platform tests that tried
    to guess what additional libraries were needed.
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@10362 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
 #
-# This script is for building and installing PyNGL.
+# This script builds PyNGL from source. Some environment variables
+# may be required. See the following comments.
+#
+
 #
 # To install, type:
 # 
@@ -15,8 +17,48 @@
 # See http://www.ncl.ucar.edu/Download/ for information on 
 # installing NCL/NCAR Graphics (available as one package).
 #
+# You may need to include Fortran system libraries (like
+# "-lgfortran" or "-lf95") to resolve undefined symbols.
+#
+# Use F2CLIBS and F2CLIBS_PREFIX for this. For example, if you
+# need to include "-lgfortran", and this library resides in /sw/lib:
+#
+#  F2CLIBS gfortran
+#  F2CLIBS_PREFIX /sw/lib
+#
 
-import sys, os, re, platform, fileinput
+# Test to make sure we actually have NumPy.
+try:
+  import numpy
+except ImportError:
+  print "Error: Cannot import NumPy. Can't continue."
+  sys.exit()
+
+import os, sys
+
+try:
+  ncarg_root = os.environ["NCARG_ROOT"]
+except:
+  print "NCARG_ROOT is not set; can't continue!"
+  sys.exit()
+
+
+# Depending on what Fortran compiler was used to build, we may need
+# additional library paths or libraries.
+try:
+  F2CLIBS = os.environ["F2CLIBS"].split()
+except:
+  F2CLIBS = ""
+
+try:
+  F2CLIBS_PREFIX = os.environ["F2CLIBS_PREFIX"]
+except:
+  F2CLIBS_PREFIX = ""
+
+
+# Done with environment variables.
+
+import re, platform, fileinput
 from distutils.core import setup, Extension
 from distutils.util import get_platform
 from distutils.sysconfig import get_python_lib
@@ -37,8 +79,10 @@ def create_version_file():
 
 # Copy the pynglex script to same filename w/the python version appended.
 def copy_pynglex_script():
-  pynglex_v_file = os.path.join(pynglex_dir,"pynglex"+sys.version[:3])
-  os.system("cp " + os.path.join(pynglex_dir,"pynglex") + " " + pynglex_v_file)
+  pynglex_script = os.path.join(pynglex_dir,"pynglex")
+  pynglex_v_file = pynglex_script + sys.version[:3]
+
+  os.system("cp " + pynglex_script + " " + pynglex_v_file)
 
 # Modify the pynglex script to have the correct python invocation.
   for line in fileinput.input(pynglex_v_file,inplace=1):
@@ -49,70 +93,67 @@ def copy_pynglex_script():
     else:
       print line,
 
-# Return list of pynglex examples and resource files.
+  return [pynglex_script, pynglex_v_file]
+
+# Copy list of pynglex examples and resource files.
 def get_pynglex_files():
   all_pynglex_files = os.listdir(pynglex_dir)
 
-  pynglex_files = []
+  dir_to_copy_to = os.path.join('ncarg','pynglex')
+  if not os.path.exists(dir_to_copy_to):
+    os.mkdir(dir_to_copy_to)
+
   for file in all_pynglex_files:
     if (file[-3:] == ".py" or file[-4:] == ".res"):
-      pynglex_files.append(os.path.join(pynglex_dir,file))
-  return pynglex_files
+      cmd = "cp " + os.path.join(pynglex_dir,file) + " " + dir_to_copy_to
+      os.system(cmd)
+      DATA_FILES.append(os.path.join(dir_to_copy_to,file))
+
+  return
 
 # Return list of files we need under $NCARG_ROOT/lib/ncarg.
 def get_ncarg_files():
   plat_dir = os.path.join("build","lib."+get_platform()+"-"+sys.version[:3], \
                           "PyNGL")
 
-  ncl_lib       = os.path.join(ncl_root,'lib')
+  ncl_lib       = os.path.join(ncarg_root,'lib')
   ncl_ncarg_dir = os.path.join(ncl_lib,'ncarg')
   ncarg_dirs    = ["colormaps","data","database","fontcaps","graphcaps"]
 
   cwd = os.getcwd()          # Retain current directory.
-  os.chdir(ncl_ncarg_dir)    # CD to $NCARG_ROOT/lib/ncarg
+  if not os.path.exists('ncarg'):
+    os.mkdir('ncarg')          # make a directory to copy files to
+  os.chdir(ncl_ncarg_dir)    # cd to $NCARG_ROOT/lib/ncarg
 
-  pyngl_ncarg_files = []
-# Walk through each directory and gather up files. Skip over
+# Walk through each directory and copy some data files. Skip over
 # the rangs directory.
   for ncarg_dir in ncarg_dirs:
     for root, dirs, files in os.walk(ncarg_dir):
-      names = []
+      dir_to_copy_to = os.path.join(cwd,'ncarg',root)
+      if not os.path.exists(dir_to_copy_to):
+        os.mkdir(dir_to_copy_to)
       for name in files:
         if root != "database/rangs":
-          names.append(os.path.join(ncl_ncarg_dir,root,name))
-      if names != []:
-        pyngl_ncarg_files.append((os.path.join(pyngl_ncarg_dir,root),names))
+          file_to_copy = os.path.join(ncl_ncarg_dir,root,name)
+          cmd = "cp " + file_to_copy + " " + dir_to_copy_to
+          os.system(cmd)
+          DATA_FILES.append(os.path.join('ncarg',root,name))
 
-  os.chdir(cwd)    # CD
+  os.chdir(cwd)    # cd back to original directory
 
-  return pyngl_ncarg_files
+# Special 'sysresfile'
+  os.system("cp sysresfile ncarg")
+  DATA_FILES.append(os.path.join('ncarg','sysresfile'))
 
-# Return list of executables we need under $NCARG_ROOT/bin.
-def get_bin_files():
-  ncl_bin         = os.path.join(ncl_root,'bin')
-  bin_files       = ["ctrans","med","psplit"]
-  pyngl_bin_files = []
-
-  for file in bin_files:
-    if(os.path.exists(os.path.join(ncl_bin,file))):
-      pyngl_bin_files.append(os.path.join(ncl_bin,file))
-
-# Add the "pynglex" script and the "pynglex.2.x" python version script.
-  pyngl_bin_files.append(os.path.join(pynglex_dir,'pynglex'))
-  pyngl_bin_files.append(os.path.join(pynglex_dir,'pynglex'+sys.version[:3]))
-
-  return pyngl_bin_files
+  return
 
 # Return list of libraries and paths needed for compilation
 def set_ncl_libs_and_paths():
-  ncl_lib = os.path.join(ncl_root,'lib')
+  PATHS = [os.path.join(ncarg_root,'lib')]
 
-  PATHS = [ncl_lib, "/usr/X11R6/lib"]
-
-  if sys.platform == "darwin":
-    dir = '/sw/lib'
-    if(os.path.exists(dir)):
-      PATHS.append(dir)
+  xdir = "/usr/X11R6/lib"
+  if(os.path.exists(xdir)):
+      PATHS.append(xdir)
 
   if (os.uname()[-1] == "x86_64" or os.uname()[-1] == "ia64"):
     dir = '/usr/X11R6/lib64'
@@ -124,31 +165,29 @@ def set_ncl_libs_and_paths():
           "ngmath", "png", "z", "X11"]
 
 # Add extra library needed for C/Fortran interfacing.
-  if platform.python_compiler() == "GCC 4.1.1":
-# You may need to change "gfortran" if you are using a different
-# Fortran compiler.
-    LIBS.append("gfortran")
-  elif sys.platform == "aix5":
+  if sys.platform == "aix5":
     LIBS.append("xlf90")
-  elif sys.platform == "darwin" and "i386" in os.uname():
-      LIBS.append('f95')
   elif sys.platform == "sunos5":
-      os.environ["CC"]="/opt/SUNWspro/bin/cc"
       LIBS.append('f77compat')
       LIBS.append('fsu')
       LIBS.append('sunmath')
-  else:
-    LIBS.append("g2c")
+
+  if F2CLIBS != "":
+    for lib in F2CLIBS:
+      LIBS.append(lib)
+
+  if F2CLIBS_PREFIX != "":
+    PATHS.append(F2CLIBS_PREFIX)
 
   return LIBS,PATHS
 
 # Return list of include paths needed for compilation
 def set_include_paths():
-  ncl_inc = os.path.join(ncl_root,'include')
+  ncl_inc = os.path.join(ncarg_root,'include')
   PATHS = [ncl_inc]
 
 # Location of numpy's "arrayobject.h".
-  PATHS.insert(0,os.path.join(pkgs_pth,"numpy/core/include"))
+  PATHS.insert(0,os.path.join(pkgs_pth,"numpy","core","include"))
 
   return PATHS
 
@@ -156,7 +195,7 @@ def set_include_paths():
 # Main section
 #----------------------------------------------------------------------
 
-long_description = "PyNGL is a Python language module designed for publication-quality visualization and analysis of scientific data. PyNGL stands for 'Python Interface to the NCL Graphics Libraries,' and it is pronounced 'pingle.' It contains the 'Nio' module, which enables NetCDF-like access for NetCDF (rw), HDF (rw), GRIB (r), and CCM (r) data files"
+long_description = "PyNGL is a Python language module designed for publication-quality visualization and analysis of scientific data. PyNGL stands for 'Python Interface to the NCL Graphics Libraries,' and it is pronounced 'pingle.'"
 
 # I read somewhere that distutils doesn't update this file properly
 # when the contents of directories change.
@@ -164,33 +203,20 @@ long_description = "PyNGL is a Python language module designed for publication-q
 if os.path.exists('MANIFEST'): os.remove('MANIFEST')
 
 PYNGL_PKG_NAME = 'PyNGL'                    # Name of package to install.
-pyngl_pth_file = [PYNGL_PKG_NAME + '.pth']  # and it's *.pth file.
+pyngl_pth_file = [PYNGL_PKG_NAME + '.pth']  # and its *.pth file.
 pkgs_pth       = get_python_lib()
 
-#
-# Make sure NCARG_ROOT is set. This is only necessary if building
-# PyNGL from source code.
-
-ncl_root = os.getenv("NCARG_ROOT")
-if ncl_root == None:
-  print "Error: NCARG_ROOT environment variable not set. Can't continue."
-  sys.exit()
-
 # Construct the version file.
-try:
-  import numpy
-except ImportError:
-  print "Error: Cannot import NumPy. Can't continue."
-  sys.exit()
 from numpy import __version__ as array_module_version
 
 pyngl_vfile = "pyngl_version.py"     # Name of version file.
 create_version_file()
 
-# Get directories of installed NCL/NCAR Graphics libraries and include files
+# Get directories of installed NCL/NCAR Graphics libraries and include
+# files
 
-LIBRARIES,LIB_PATHS = set_ncl_libs_and_paths()
-INC_PATHS           = set_include_paths()
+LIBRARIES,LIB_DIRS = set_ncl_libs_and_paths()
+INC_DIRS           = set_include_paths()
 
 # Set some compile options.
 if os.uname()[-1] == "x86_64" or \
@@ -202,14 +228,14 @@ DMACROS =  [('NeedFuncProto', None)]
 EXT_MODULES = [Extension('_hlu', 
               ['Helper.c','hlu_wrap.c','gsun.c'],
                 define_macros   = DMACROS,
-                include_dirs    = INC_PATHS,
-                library_dirs    = LIB_PATHS,
+                include_dirs    = INC_DIRS,
+                library_dirs    = LIB_DIRS,
                 libraries       = LIBRARIES),
                Extension('fplib', 
                [os.path.join('paft','fplibmodule.c')],
                 define_macros   = DMACROS,
-                include_dirs    = INC_PATHS,
-                library_dirs    = LIB_PATHS,
+                include_dirs    = INC_DIRS,
+                library_dirs    = LIB_DIRS,
                 libraries       = LIBRARIES)]
 
 # Set the directories of where the extra PyNGL data files (fontcaps,
@@ -220,18 +246,13 @@ pyngl_data_dir  = os.path.join(pyngl_ncarg_dir, 'data')
 pynglex_dir     = "examples"
 python_bin_dir  = os.path.join(sys.prefix,'bin')
 
-pynglex_files = get_pynglex_files()   # Get example files associated
-                                      # with the "pynglex" script.
-
-copy_pynglex_script()  # Copy pynglex script to itself with
-                       # python version appended
-
 # Create list of supplemental files needed.
-DATA_FILES = get_ncarg_files()          # We need NCARG_ROOT for the lib
-DATA_FILES.append((python_bin_dir,get_bin_files()))      # and bin files.
-DATA_FILES.append((pkgs_pth, pyngl_pth_file))
-DATA_FILES.append((pyngl_ncarg_dir, ['sysresfile']))
-DATA_FILES.append((os.path.join(pyngl_ncarg_dir,'pynglex'),pynglex_files))
+DATA_FILES = []
+get_pynglex_files()   # Get example files associated
+                      # with the "pynglex" script.
+pynglex_scripts = copy_pynglex_script()  # Copy pynglex script to itself with
+                                         # python version appended
+get_ncarg_files()                        # We need NCARG_ROOT for the lib
 
 setup (name = 'PyNGL',
        version          = '1.3.0',
@@ -244,10 +265,11 @@ setup (name = 'PyNGL',
        description      = '2D visualization library',
        long_description = long_description,
        url              = 'http://www.pyngl.ucar.edu/',
-       package_dir      = { PYNGL_PKG_NAME : ''},
-       packages         = [PYNGL_PKG_NAME],
-       data_files       = DATA_FILES,
+       package_dir      = { PYNGL_PKG_NAME : '.'},
+       packages         = [ PYNGL_PKG_NAME ],
+       package_data     = { PYNGL_PKG_NAME : DATA_FILES },
+       data_files       = [(pkgs_pth, pyngl_pth_file)],
+       scripts          = pynglex_scripts,
        ext_package      = PYNGL_PKG_NAME,
        ext_modules      = EXT_MODULES
-    )
-
+)
