ab9f54acf96bd46ec4c74c57a31b37f5df3431cb
Mary Haley <haley@ucar.edu>
2008-10-11 21:43:58 +0000

    This example was not producing the create image with regard to the labelbar.
    This was because the labelbar being created was supposed to be
    associated with the map plot, and not the contour plot (which is a bit
    unusual).
    
    To fix this, I moved some of the resources inside the cn12p.res file
    to inside the script. I also modified the script so that instead
    of using Ngl.contour_map, it now uses Ngl.contour, Ngl.map, and
    Ngl.overlay.  This allows the labelbar resources to be applied to
    the map's labelbar (allowing more control over the labelbar, like
    how many boxes it contains).
    
    
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@10450 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- examples/cn12p.py
+++ examples/cn12p.py
@@ -9,7 +9,7 @@
 #     Contours over maps
 #
 #   Author:
-#     Fred Clare (based on examples of Mary Haley and Dave Kennison).
+#     Fred Clare (based on examples of Dave Kennison and Mary Haley).
 #
 #   Date of initial publication:    
 #     September, 2004
@@ -23,36 +23,39 @@
 #  Effects illustrated
 #     o  Generating test data using generate_2d_array.
 #     o  Drawing color filled contours over specified geographic areas.
-#     o  Using a ".res" file to set many resources, for example to:
-#          + set fill colors
-#          + turn off high labels, info label, line labels, low labels.
-#          + turn on area masking.
-#          + set the map projection to "orthographic".
-#          + set several label bar resources.
+#     o  Using a map labelbar instead of a contour labelbar
 #
 #  Output:
 #     A single visualization is produced.
 #
 #  Notes: 
 #     This example requires the resource file cn12p.res.
-
 #
-#  Import numpy and sys.
+#     Resource files should be used carefully, because you want to
+#     make sure the resources apply to the correct objects. Resources
+#     set in a script override any resources set in a resource file.
+#
+#     The resources for this example are split between a resource 
+#     file and this script. It is mostly by choice which resources
+#     appear where.
+# 
+#     You cannot set ngl* resources in a resource file.
+#
+#  Import numpy
 #
 import numpy
-import sys
 
 #
 #  Import Ngl support functions.
 #
 import Ngl
 
-N = 40
-len_dims = [N,N]
 #
 #  These are the geographical areas we want to fill.
 #
-fill_specs = ["water","land"]
+mp_fill_specs = ["water","land"]
+mp_fill_colrs = [7,2]              # blue, gray
+
 #
 #  These are the geographical areas we want to mask.
 #
@@ -66,25 +69,86 @@ mask_specs =                                                                 \
   "somalia","south-africa","sudan","swaziland","tanzania","togo","tunisia",  \
   "uganda","upper-volta","western-sahara","zaire","zambia","zimbabwe"]
 
+cn_fill_clrs = [3,4,5,6,8,9,10,11,12,13,14,15]
+
+#
+# Generate color map using RGB values.
+#
+cmap = numpy.array([[1.00,1.00,1.00],[0.00,0.00,0.00],[0.70,0.70,0.70], \
+                    [0.75,0.50,1.00],[0.50,0.00,1.00],[0.00,0.00,1.00], \
+                    [0.00,0.50,1.00],[0.00,1.00,1.00],[0.00,1.00,0.60], \
+                    [0.00,1.00,0.00],[0.70,1.00,0.00],[1.00,1.00,0.00], \
+                    [1.00,0.75,0.00],[1.00,0.38,0.38],[1.00,0.00,0.38], \
+                    [1.00,0.00,0.00]])
 #
 #  Open a workstation.
 #
+wres            = Ngl.Resources()
+wres.wkColorMap = cmap
 wks_type = "ps"
-wks = Ngl.open_wks(wks_type,"cn12p")
+wks = Ngl.open_wks(wks_type,"cn12p",wres)
 
+# Generate some dummy data
 dirc = Ngl.pynglpath("data")
 z    = Ngl.generate_2d_array ([40,40],15,15,-10.,110.)
  
-resources = Ngl.Resources()
-resources.sfXCStartV  = -18.0
-resources.sfXCEndV    =  52.0
-resources.sfYCStartV  = -35.0
-resources.sfYCEndV    =  38.0
-resources.vpXF        =   0.1
-resources.mpMaskAreaSpecifiers  =  mask_specs
-resources.mpFillAreaSpecifiers  =  fill_specs
-resources.pmLabelBarDisplayMode =  "always"
-Ngl.contour_map(wks,z[:,:],resources)
+# Create contour and map resource lists.
+cnres = Ngl.Resources()
+mpres = Ngl.Resources()
+cnres.nglDraw  = False
+cnres.nglFrame = False
+mpres.nglDraw  = False
+mpres.nglFrame = False
 
-del resources
+#
+# Map fill resources.
+#
+# To fill map areas, you can first specify a general set
+# of areas (mpFillBoundarySets) and then a specific set of
+# areas. This allows you to indicate what general areas you always
+# want filled, and then what specific areas on top of that you
+# also want filled.
+#
+# Here, no general areas are being specified, but the specific
+# areas of "water" and "land" are. This is because we can then
+# use the mpMaskAreaSpecifiers resource to indicate which of
+# the land and/or water areas you don't want to fill.
+#
+mpres.mpFillOn              = True               # Turn on map fill
+mpres.mpFillBoundarySets    = "NoBoundaries"   
+mpres.mpFillAreaSpecifiers  = mp_fill_specs      # water, land
+mpres.mpSpecifiedFillColors = mp_fill_colrs      #  blue, gray
+
+# The mpMaskAreaSpecifiers array allows you to specify which areas are
+# not to be filled in the areas given by mpFillAreaSpecifiers.
+mpres.mpAreaMaskingOn       = True
+mpres.mpMaskAreaSpecifiers  =  mask_specs      # areas to mask (protect)
+
+#
+# This set of resources deals with a map labelbar.
+#
+# Note that this is different from a contour labelbar
+# (which we will turn off below), and we have more 
+# control over it (i.e. we can set the number of boxes,
+# the fill # colors, etc) than we do with a contour labelbar.
+#
+mpres.pmLabelBarDisplayMode =  "Always"        # Turn on a map labelbar
+
+# Labelbar resources.
+mpres.lbLabelStrings        = ["Ocean","Land","< 0","0-10","10-20","20-30",\
+                               "30-40","40-50","50-60","60-70","70-80",    \
+                               "80-90","90-100","> 100"]
+mpres.lbFillColors          = mp_fill_colrs + cn_fill_clrs
+
+# Fill resources
+cnres.cnFillColors          =  cn_fill_clrs
+cnres.pmLabelBarDisplayMode = "Never"  # turn off, b/c map has one turned on
+
+contour = Ngl.contour(wks,z[:,:],cnres)
+map     = Ngl.map(wks,mpres)
+
+Ngl.overlay(map,contour)
+Ngl.draw(map)
+Ngl.frame(wks)
+
 Ngl.end()
