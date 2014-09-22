1cd47951b48e4032fc3068f490ac3a3ee55cc37a
Mary Haley <haley@ucar.edu>
2014-09-22 16:11:08 +0000

    Added code for subtitles.
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@15781 10fe2890-8a40-4696-8b1d-7ccbb19c398e

--- examples/mpas2.py
+++ examples/mpas2.py
@@ -26,7 +26,7 @@
 #    o  Plotting unstructured data
 # 
 #  Output:
-#    This example produces two contour plots.
+#    This example produces one or two contour plots.
 #     
 #  Notes:
 #     The MPAS data file is too large to be included with this 
@@ -39,9 +39,63 @@
 import os, numpy, math, time, sys
 import Ngl, Nio
 
+#----------------------------------------------------------------------
+# Function to add three subtitles to the top of a plot, left-justified,
+# centered, and right-justified.
+#----------------------------------------------------------------------
+def subtitles(wks, plot, left_string, center_string, right_string):
+  ttres         = Ngl.Resources()
+  ttres.nglDraw = False          # Make sure string is just created, not drawn.
+#
+# Retrieve font height of left axis string and use this to calculate
+# size of subtitles.
+#
+  font_height = Ngl.get_float(plot.base,"tiXAxisFontHeightF")
+  ttres.txFontHeightF = font_height*0.8    # Slightly smaller
+
+#
+# Set some some annotation resources to describe how close text
+# is to be attached to plot.
+#
+  amres = Ngl.Resources()
+  amres.amOrthogonalPosF = -0.51   # Top of plot plus a little extra
+                                   # to stay off the border.
+#
+# Create three strings to put at the top, using a slightly
+# smaller font height than the axis titles.
+#
+  if left_string != "":
+    txidl = Ngl.text(wks, plot, left_string, 0., 0., ttres)
+
+    amres.amJust         = "BottomLeft"
+    amres.amParallelPosF = -0.5   # Left-justified
+    annoidl              = Ngl.add_annotation(plot, txidl, amres)
+
+  if center_string != "":
+    txidc = Ngl.text(wks, plot, center_string, 0., 0., ttres)
+
+    amres.amJust         = "BottomCenter"
+    amres.amParallelPosF = 0.0   # Centered
+    annoidc              = Ngl.add_annotation(plot, txidc, amres)
+
+  if right_string != "":
+    txidr = Ngl.text(wks, plot, right_string, 0., 0., ttres)
+
+    amres.amJust         = "BottomRight"
+    amres.amParallelPosF = 0.5   # Right-justifed
+    annoidr              = Ngl.add_annotation(plot, txidr, amres)
+
+  return
+
+#----------------------------------------------------------------------
+# Function to print the min/max of a given variable.
+#----------------------------------------------------------------------
 def print_min_max(var,varname):
   print(varname + ": min/max = %.2f " % numpy.min(var) + "/" + "%.2f" % numpy.max(var))
 
+#----------------------------------------------------------------------
+# Main code
+#----------------------------------------------------------------------
 mpas_file = "x1.2621442.partial.nc"
 if(not os.path.exists(mpas_file)):
   print "You do not have the necessary file to run this example."
@@ -49,9 +103,11 @@ if(not os.path.exists(mpas_file)):
   sys.exit()
 
 #---Read a timestep of "t2m" 
-nt  = 3                                        # nt=0 is a constant field for t2m
-f   = Nio.open_file(mpas_file)
-t2m = f.variables["t2m"][nt,:]
+varname  = "t2m"
+ntime    = 3                                        # ntime=0 is a constant field for t2m
+f        = Nio.open_file(mpas_file)
+t2m      = f.variables[varname][ntime,:]
+time_str = ''.join(f.variables["xtime"][ntime][:]).strip()
 
 lonCell        = f.variables["lonCell"][:]
 latCell        = f.variables["latCell"][:]
@@ -92,10 +148,13 @@ print_min_max(latvoc,"latvoc")
 print_min_max(lonvoc,"lonvoc")
 
 #---Start the graphics
-wks_type = "ps"
+wks_type = "x11"
 wks = Ngl.open_wks(wks_type,"mpas2")
 
 res                      = Ngl.Resources()              # Plot mods desired.
+
+res.nglDraw              = False             # Turn off plot draw and frame advance. We will
+res.nglFrame             = False             # do it later after adding subtitles.
 
 res.cnFillOn             = True              # color plot desired
 res.cnFillPalette        = "ncl_default"
@@ -126,12 +185,23 @@ res.sfYArray             = latCell
 # Raster fill plot
 #----------------------------------------------------------------------
 res.cnFillMode           = "RasterFill"
-res.tiMainString         = "MPAS grid, " + str(nCells) + " cells (raster fill)"
-res.tiMainOffsetYF       = -0.03      # Move main title towards plot
+res.tiMainString         = "MPAS grid - raster fill"
+res.tiMainOffsetYF       = 0.03      # Move main title towards plot
 
-t1   = time.time()
+t1 = time.time()
 plot = Ngl.contour_map(wks,t2m,res)  
-t2   = time.time()
+
+#---Attach some subtitles to the plot
+left_string   = varname
+center_string = time_str
+right_string  = str(nCells) + " cells"
+subtitles(wks,plot,left_string,center_string,right_string)
+
+#---Drawing the plot will also draw the subtitles
+Ngl.draw(plot)
+Ngl.frame(wks)
+
+t2 = time.time()
 print "RasterFill took %0.3f seconds" % (t2-t1)
 
 # Clean up, otherwise next plot gets drawn on top of this one (?)
@@ -141,17 +211,30 @@ del plot
 # Cell fill plot
 #
 # NOTE: This plot causes a large output file to be created, which may 
-# take a while to view.
+# take a while to view. This is turned off for now.
+#
+# The cell fill plot should look a little better than the raster fill
+# because it will fill the corners of the map plot. Raster fill leaves
+# little gaps in these areas.
 #----------------------------------------------------------------------
-res.cnFillMode           = "CellFill"
-res.sfXCellBounds        = lonvoc       # necessary for CellFill 
-res.sfYCellBounds        = latvoc
-res.tiMainString         = "MPAS grid, " + str(nCells) + " cells (cell fill)"
+CELL_FILL_PLOT = False
+if CELL_FILL_PLOT:
+  res.cnFillMode           = "CellFill"
+  res.sfXCellBounds        = lonvoc       # necessary for CellFill 
+  res.sfYCellBounds        = latvoc
+  res.tiMainString         = "MPAS grid - cell fill"
 
-t1   = time.time()
-plot = Ngl.contour_map(wks,t2m,res)  
-t2   = time.time()
-print "CellFill took %0.3f seconds" % (t2-t1)
+  t1 = time.time()
+  plot = Ngl.contour_map(wks,t2m,res)  
+
+  subtitles(wks,plot,left_string,center_string,right_string)
+
+#---Drawing the plot will also draw the subtitles
+  Ngl.draw(plot)
+  Ngl.frame(wks)
+
+  t2 = time.time()
+  print "CellFill took %0.3f seconds" % (t2-t1)
 
 Ngl.end()
 
