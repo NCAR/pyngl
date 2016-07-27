1894e8dca7ec05d6dc6172786d0422ce2d7ea64b
Mary Haley <haley@ucar.edu>
2016-07-27 19:37:59 +0000

    Added NUG examples to PyNGL example suite (Thanks Karin!)
    git-svn-id: https://subversion.ucar.edu/ncldev/pyngl/trunk@16586 10fe2890-8a40-4696-8b1d-7ccbb19c398e

---
+++ examples/NUG_curvilinear_contour_PyNGL.py
@@ -0,0 +1,71 @@
+"""
+  NCL User Guide Python Example:   PyNGL_curvilinear_contour.py
+
+   - curvilinear data
+   - colormap
+   - draw edges
+ 
+ 04.06.15  kmf
+"""
+import Ngl,Nio
+import os,sys
+
+#--  define variables
+diri   = "./"                                 #-- data directory
+fname  = "tos_ocean_bipolar_grid.nc"          #-- curvilinear data 
+
+#---Test if file exists
+if(not os.path.exists(diri+fname)):
+  print("You do not have the necessary file (%s) to run this example." % (diri+fname))
+  print("You can get the files from the NCL website at:")
+  print("http://www.ncl.ucar.edu/Document/Manuals/NCL_User_Guide/Data/")
+  sys.exit()
+
+#--  open file and read variables
+f      = Nio.open_file(diri + fname,"r")
+var    = f.variables["tos"][0,:,:]            #-- first time step, reverse latitude
+lat2d  = f.variables["lat"][:,:]              #-- 2D latitudes 
+lon2d  = f.variables["lon"][:,:]              #-- 2D longitudes
+
+#-- open a workstation
+wks_type              = "png"                 #-- output type
+wks_name              = os.path.basename(__file__).split(".")[0]
+wks                   =  Ngl.open_wks(wks_type,wks_name)
+
+#-- set resources
+res                   =  Ngl.Resources()      #-- generate an resources object for plot
+
+res.cnFillOn          =  True                 #-- turn on contour fill
+res.cnLinesOn         =  False                #-- don't draw contour lines
+res.cnLineLabelsOn    =  False                #-- don't draw line labels
+res.cnFillPalette     = "BlueWhiteOrangeRed"  #-- set color map
+res.cnFillMode        = "CellFill"            #-- change contour fill mode
+res.cnCellFillEdgeColor =  "black"            #-- edges color
+res.cnCellFillMissingValEdgeColor = "gray50"  #-- missing value edges color
+res.cnMissingValFillColor = "gray50"          #-- missing value fill color
+
+res.lbOrientation     = "Horizontal"          #-- labelbar orientation
+
+res.tiMainString      = "Curvilinear grid:  MPI-ESM-LR  (2D lat/lon arrays)"  #-- title string
+res.tiMainFontHeightF =  0.022                #-- main title font size
+
+res.sfXArray          =  lon2d                #-- longitude grid cell center
+res.sfYArray          =  lat2d                #-- latitude grid cell center
+
+res.mpFillOn          =  False                #-- don't draw filled map
+res.mpGridLatSpacingF =  10.                  #-- grid lat spacing
+res.mpGridLonSpacingF =  10.                  #-- grid lon spacing
+
+res.mpDataBaseVersion = "MediumRes"           #-- map database
+res.mpLimitMode       = "LatLon"              #-- must be set using minLatF/maxLatF/minLonF/maxLonF
+res.mpMinLatF         = -10.                  #-- sub-region minimum latitude
+res.mpMaxLatF         =  80.                  #-- sub-region maximum latitude
+res.mpMinLonF         = -120.                 #-- sub-region minimum longitude
+res.mpMaxLonF         =  60.                  #-- sub-region maximum longitude
+
+#-- create the plot
+plot = Ngl.contour_map(wks,var,res)           #-- create the contour plot
+
+#-- end
+
+Ngl.end()
---
+++ examples/NUG_rectilinear_contour_PyNGL.py
@@ -0,0 +1,70 @@
+"""
+  NCL User Guide Python Example:   PyNGL_rectilinear_contour.py
+
+   - filled contour over map plot
+   - rectilinear grid (lat/lon)
+   - colorbar
+   
+  2015-06-04  kmf
+"""
+import Ngl,Nio
+import os,sys
+
+#-- define variables
+diri   = "./"                                  #-- data directory
+fname  = "rectilinear_grid_2D.nc"              #-- data file name
+
+#---Test if file exists
+if(not os.path.exists(diri+fname)):
+  print("You do not have the necessary file (%s) to run this example." % (diri+fname))
+  print("You can get the files from the NCL website at:")
+  print("http://www.ncl.ucar.edu/Document/Manuals/NCL_User_Guide/Data/")
+  sys.exit()
+
+minval =  250.                                 #-- minimum contour level
+maxval =  315                                  #-- maximum contour level
+inc    =    5.                                 #-- contour level spacing
+
+#-- open file and read variables
+f      =  Nio.open_file(diri + fname,"r")      #-- open data file
+temp   =  f.variables["tsurf"][0,:,:]          #-- first time step
+lat    =  f.variables["lat"][:]                #-- all latitudes
+lon    =  f.variables["lon"][:]                #-- all longitudes
+
+tempac,lon =  Ngl.add_cyclic(temp,lon)
+
+#-- open a workstation
+wkres                 =  Ngl.Resources()      #-- generate an res object 
+wks_type              = "png"                 #-- output type
+wks_name              = os.path.basename(__file__).split(".")[0]
+wks                   =  Ngl.open_wks(wks_type,wks_name,wkres)
+
+#-- set resources
+res                    =  Ngl.Resources()      #-- generate an resource object for plot
+
+if hasattr(f.variables["tsurf"],"long_name") and hasattr(f.variables["tsurf"],"units"):
+   res.tiMainString = "%s (%s)" % (f.variables["tsurf"].long_name,\
+                                   f.variables["tsurf"].units)          #-- set main title
+
+res.cnFillOn              =  True              #-- turn on contour fill.
+res.cnLinesOn             =  False             #-- turn off contour lines
+res.cnLineLabelsOn        =  False             #-- turn off line labels.
+res.cnInfoLabelOn         =  False             #-- turn off info label.
+res.cnLevelSelectionMode  = "ManualLevels"     #-- select manual level selection mode
+res.cnMinLevelValF        =  minval            #-- minimum contour value
+res.cnMaxLevelValF        =  maxval            #-- maximum contour value
+res.cnLevelSpacingF       =  inc               #-- contour increment
+res.cnFillPalette         = "rainbow"          #-- choose color map
+
+res.mpGridSpacingF        =  30                #-- map grid spacing
+
+res.sfXArray              =  lon               #-- longitude locations of data
+res.sfYArray              =  lat               #-- latitude locations of data
+
+res.lbOrientation         = "Horizontal"       #-- labelbar orientation
+
+map = Ngl.contour_map(wks,tempac,res)          #-- draw contours over a map.
+
+#-- end
+Ngl.end()
+
